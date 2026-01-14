5f0f7240f6752437349e86fc2c02c3e4f0ed1d4f
yashwanth9182 <mallepallyyashwanth123@gmail.com>
2026-01-14 12:23:22 +0530

    Final Render prep

--- event_project/event_project/settings.py
+++ event_project/event_project/settings.py
@@ -17,8 +17,7 @@ import dj_database_url
 import os
 
 
-# DEBUG = os.getenv("DEBUG","False")=="True"
-DEBUG = "True"
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
