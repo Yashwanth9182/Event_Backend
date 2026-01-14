594584560256ccf036237f3e6ef1cbce4e0bfa23
yashwanth9182 <mallepallyyashwanth123@gmail.com>
2026-01-14 11:44:07 +0530

    Final deployment prep - all packages ready

--- event_project/event_project/settings.py
+++ event_project/event_project/settings.py
@@ -15,9 +15,8 @@ from decouple import config
 import dj_database_url
 
 import os
-from dotenv import load_dotenv
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+
+
 # DEBUG = os.getenv("DEBUG","False")=="True"
 DEBUG = "True"
 
